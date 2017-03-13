a01ec9218581ccb0b6274a798160a61adbe17662
Jan Kuri <jkuri88@gmail.com>
2017-03-13 11:34:35 +0100

    fix(): enable lazy module back

--- src/app/+lazy/lazy.module.ts
+++ src/app/+lazy/lazy.module.ts
@@ -1,21 +1,21 @@
-// import {NgModule, Component} from '@angular/core'
-// import {RouterModule} from '@angular/router'
-//
-//
-// @Component({
-// 	selector: 'lazy-view',
-// 	template: `<h3>i'm lazy</h3>`
-// })
-// export class LazyView {}
-//
-// @NgModule({
-// 	declarations: [LazyView],
-// 	imports: [
-// 		RouterModule.forChild([
-// 			{ path: '', component: LazyView, pathMatch: 'full'}
-// 		])
-// 	]
-// })
-// export class LazyModule {
-//
-// }
+import {NgModule, Component} from '@angular/core'
+import {RouterModule} from '@angular/router'
+
+
+@Component({
+	selector: 'lazy-view',
+	template: `<h3>i'm lazy</h3>`
+})
+export class LazyView {}
+
+@NgModule({
+	declarations: [LazyView],
+	imports: [
+		RouterModule.forChild([
+			{ path: '', component: LazyView, pathMatch: 'full'}
+		])
+	]
+})
+export class LazyModule {
+
+}
